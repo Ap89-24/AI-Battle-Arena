61f214991503cfa05a39d071d8b3eae9f2620c13
Ap89-24 <aman082199@gmail.com>
2026-07-24 02:28:07 +0530

    config setup and api key setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/health", (req, res) => {
-    res.status(200).json({
-        status: "ok"
-    })
-})
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+  });
+});
 
 export default app;
