258779be0c220381b89770f31c8e3afd5d080eab
Ap89-24 <aman082199@gmail.com>
2026-08-04 02:01:53 +0530

    creating invoke api

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,15 +3,16 @@ import runGraph from "./Ai/graph.ai.services.js";
 
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-  });
-});
 
-app.post("/ai-chat", async (req, res) => {
-  const result = await runGraph("give me the factorial in javascript???");
-  res.status(200).json(result);
+
+app.post("/invoke", async (req, res) => {
+  const { prompt } = req.body;
+  const result = await runGraph(prompt);
+  res.status(200).json({
+    message: "Graph AI invoked successfully",
+    success: true,
+    data: result
+  });
 });
 
 export default app;
